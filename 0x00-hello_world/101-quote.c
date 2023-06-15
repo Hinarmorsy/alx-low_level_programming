19d9d1bc1baf402a32fc4932204c0950d817bc08
Hinarmorsy <hinar.morsy@gmail.com>
2023-06-15 12:55:05 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  * Description: C program prints exactly and that piece of art is useful
  *
  * Return: 1 (error)
-*/
+ */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
-        return (1);
+	 return (1);
 }
