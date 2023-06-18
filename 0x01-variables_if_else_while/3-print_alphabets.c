bcd70a2871101aa0326cc52ec26dc64bf40051fa
Hinarmorsy <hinar.morsy@gmail.com>
2023-06-18 15:18:55 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description:program that prints the alphabet in lowercase, and in uppercase
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (ch <= 'Z')
+	{
+		putchar(CH);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
