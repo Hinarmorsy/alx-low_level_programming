e4f5525c08d920ddf02a33d5625f6c9ca1d03256
Hinarmorsy <hinar.morsy@gmail.com>
2023-06-19 00:50:35 -0700

    12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints all possible comb of 2 two-digit
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
