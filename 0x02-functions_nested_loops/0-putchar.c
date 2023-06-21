12749d4986a7015f8692f54411d00da784e32d4a
Hinarmorsy <hinar.morsy@gmail.com>
2023-06-21 13:07:37 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ *Description: Write a program that prints _putchar
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
