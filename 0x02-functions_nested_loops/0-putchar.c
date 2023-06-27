1779acb62a12a874ee24bb8863b148aa0eec5126
Hinarmorsy <hinar.morsy@gmail.com>
2023-06-27 13:10:25 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,12 @@
  * Return: 0
  */
 
-
 int main(void)
 {
-	char str[] = "_ptchar";
+	char str[] = "_putchar";
 	int ch;
 	for (ch = 0; ch < 8;ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
-
 	return (0);
 }
-
