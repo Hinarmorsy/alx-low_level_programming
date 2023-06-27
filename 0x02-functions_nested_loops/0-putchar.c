aca3477b21302c6c198360a4312bc08720471c43
Hinarmorsy <hinar.morsy@gmail.com>
2023-06-27 13:08:42 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,14 @@
  */
 
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+int main(void)
+{
+	char str[] = "_ptchar";
+	int ch;
+	for (ch = 0; ch < 8;ch++)
+		_putchar(str[ch]);
 	_putchar('\n');
+
 	return (0);
 }
 
