3f717c64ef9020e11ebda53bf472d6f6c1ce4a29
Hinarmorsy <hinar.morsy@gmail.com>
2023-07-01 12:45:17 -0700

    9

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,7 @@
 * print_times_table - prints the n times table, starting with 0
 * @n: number of the times table
 */
+
 	void print_times_table(int n)
 	{
 		int i, j, k;
@@ -34,4 +35,4 @@
 						_putchar((k / 10) + '0');
 						_putchar((k % 10) + '0');
 					} else if (k >= 100)
-					{
+	}					{
